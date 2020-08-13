ac4f61f6b2da2031df54d7af4633428d38804dc7
DatBiscuit <snowjenjen@gmail.com>
2020-08-13 09:49:02 -0700

    Update centered.py

--- Projects/Challenge_Problems/Pyramid/centered.py
+++ Projects/Challenge_Problems/Pyramid/centered.py
@@ -7,7 +7,7 @@
 # for loop
 
 for k in range(1,6):
-    for space in range(0,k-6):
+    for space in range(0,5-k):
         print(" ", end="")
     for l in range(k):
         print("* ", end="")
@@ -19,7 +19,7 @@ i = 1
 j = 0
 while i < 6:
     j = 0
-    space = 5 - j
+    space = 5 - i
     while space > 0:
         print(" ", end="")
         space -= 1
